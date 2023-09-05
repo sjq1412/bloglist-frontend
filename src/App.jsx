9f60cc1111452f7e0f94acc895fd76258ddf7e38
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-05 21:56:46 +0800

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import "./index.css"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState({ message: null, variant: null })
+
+  useEffect(() => {
+    const clearNotification = () => setTimeout(() => {
+      setNotification(null)
+    }, 3000);
+    
+    if (notification) {
+      clearNotification()
+    }
+    return () => {
+      clearTimeout(clearNotification)
+    }
+  }, [notification])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,10 +31,16 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <Notification message={notification?.message} variant={notification?.variant} />
+      {!user && <LoginForm setUser={setUser} setNotification={setNotification} />}
+      {
+        user && <div>
+          <h2>blogs</h2>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div> 
+      }
     </div>
   )
 }
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import loginService from "../services/login"
+
+const LoginForm = ({setUser, setNotification}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log({username, password})
+    try {
+        const user = await loginService.login({ username, password })
+        setUser(user)
+        setUsername('')
+        setPassword('')
+    } catch(exception) {
+        console.error({exception})
+        setNotification({ message: 'Wrong credentials', variant: 'error' })
+    }
+  }
+
+  return (
+    <div>
+        <h1>log in to application</h1>
+        <form onSubmit={handleLogin}>
+            <div>username 
+                <input 
+                    type='text' 
+                    name="username" 
+                    value={username} 
+                    onChange={({target}) => setUsername(target.value)} 
+                />
+            </div>
+            <div>password 
+                <input 
+                    type='text' 
+                    name="password" 
+                    value={password} 
+                    onChange={({target}) => setPassword(target.value)} 
+                />
+            </div>
+            <button type='submit'>login</button>
+        </form>
+    </div>
+  )
+}
+
+export default LoginForm
