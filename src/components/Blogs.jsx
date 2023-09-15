e431b07d7e282b822e4ba08a5231ed6466b1a2c2
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-15 11:30:03 +0800

    sort blogs based on likes

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Blog from './Blog'
 
 const Blogs = ({blogs, like}) => {
+  const sortedBlogs = blogs.sort((a, b) => (b.likes || 0) - (a.likes || 0))
+
   return (
-    <div>{blogs.map(blog =>
+    <div>{sortedBlogs.map(blog =>
         <Blog key={blog.id} blog={blog} like={like} />
       )}</div>
   )
