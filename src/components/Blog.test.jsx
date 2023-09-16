fc33d332b2788d37d958b901b3b2bf4cc7c2610c
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-16 18:09:52 +0800

    test Blog component

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -4,13 +4,15 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog component', () => {
-  test('title and author are rendered, but does not render url and likes by default', () => {
+  let container
+  let mockHandler
+
+  beforeEach(() => {
     const user = {
       id: 'userId',
       username: 'sarah',
       name: 'Sarah Jane'
     }
-
     const blog = {
       title: 'Test Blog',
       author: 'Sarah Jane',
@@ -18,14 +20,39 @@ describe('Blog component', () => {
       likes: 24,
       user
     }
+    mockHandler = jest.fn()
 
+    container = render(<Blog blog={blog} user={user} like={mockHandler} />).container
+  })
 
-    const { container } = render(<Blog blog={blog} user={user} />)
-
+  test('title and author are rendered, but does not render url and likes by default', () => {
     const blogText = screen.getByText('Test Blog Sarah Jane')
     expect(blogText).toBeDefined()
 
     const blogDetails = container.querySelector('.blogDetails')
     expect(blogDetails).toHaveStyle('display: none')
+  })
+
+  test('URL and number of likes are shown when view button has been clicked', async () => {
+    const blogDetails = container.querySelector('.blogDetails')
+    expect(blogDetails).toHaveStyle('display: none')
+
+    const mockUser = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await mockUser.click(viewButton)
+
+    expect(blogDetails).not.toHaveStyle('display: none')
+  })
+
+  test('event handler will be called twice when like button is clicked twice', async () => {
+    const mockUser = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await mockUser.click(viewButton)
+
+    const likeButton = container.querySelector('.likeButton')
+    await mockUser.click(likeButton)
+    await mockUser.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
